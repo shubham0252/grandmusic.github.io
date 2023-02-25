267c06d6bb8166559b5240ec56951db2f014ea1b
shubham0252 <shubham931204@gmail.com>
2023-02-25 10:37:16 +0530

    music app
    this is my first project

---
+++ Miz Music/songsArray.js
@@ -0,0 +1,334 @@
+// let data1 =
+// {
+//     Sidhu_Moose_Wala: [
+//         {
+//             songName: "Bitch_i'm_Back.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "bitch.jpg"
+//         },
+//         {
+//             songName: "Regret.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "regret.jpg"
+//         },
+//         {
+//             songName: "Vaar.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "vaar.jpg"
+//         },
+//         {
+//             songName: "Sin.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "sin.jpg"
+//         },
+//         {
+//             songName: "Burberry.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "burberry.jpg"
+//         },
+//         {
+//             songName: "Moosetape_Intro.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "moosetape.jpg"
+//         },
+//         {
+//             songName: "US.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "us.jpg"
+//         },
+//         {
+//             songName: "Unfuckwithable.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "unfuck.jpg"
+//         },
+//         {
+//             songName: "Rack_And_Rounds.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "rack.jpg"
+//         },
+//         {
+//             songName: "295.mp3",
+//             singerName: "Sidhu Moose Wala",
+//             image: "295.jpg"
+//         }
+//     ],
+
+//     Honey_Singh: [
+//         {
+//             songName: "Birthday_Bash_1.mp3",
+//             singerName: "Honey Singh",
+//             image: "birthday.jpg"
+//         },
+//         {
+//             songName: "Call_Aundi_1.mp3",
+//             singerName: "Honey Singh",
+//             image: "call.jpg"
+//         },
+//         {
+//             songName: "Saiyaan_Ji_1.mp3",
+//             singerName: "Honey Singh",
+//             image: "saiyaan.webp"
+//         },
+//         {
+//             songName: "Love_Dose_Extended.mp3",
+//             singerName: "Honey Singh",
+//             image: "love.jpg"
+//         }
+//     ],
+//     Khan_Bhaini: [
+//         {
+//             songName: "Gaddi_Pichhe_Naa_1.mp3",
+//             singerName: "Khan Bhaini",
+//             image: "gaddi.jpg"
+//         },
+//         {
+//             songName: "Sun_Fer_1.mp3",
+//             singerName: "Khan Bhaini",
+//             image: "sun.jpg"
+//         },
+//         {
+//             songName: "Chak_Chak_Chak_1.mp3",
+//             singerName: "Khan Bhaini",
+//             image: "chak.jpg"
+//         },
+//         {
+//             songName: "Churi_1.mp3",
+//             singerName: "Khan Bhaini",
+//             image: "Churi.jpg"
+//         }
+//     ]
+// }
+
+let data2 = [
+    {
+        Name: "Sidhu Moose Wala",
+        img: "sidhuWallpaper.jpg"
+    },
+    {
+        Name: "Khan Bhaini",
+        img: "khan.jpg"
+    },
+    {
+        Name: "Honey Singh",
+        img: "honey.jpg"
+    },
+    {
+        Name: "Amrit Maan",
+        img: "amritmaan.jpg"
+    },
+    {
+        Name: "Ranjit Bawa",
+        img: "rajitbawa.jpg"
+    },
+    {
+        Name: "Karan Aujla",
+        img: "karan.jpg"
+    },
+    {
+        Name: "Jordan Sandu",
+        img: "jordan.jpg"
+    },
+    {
+        Name: "Satinder Sartaj",
+        img: "satinder.jpg"
+    },
+    {
+        Name: "Diljit Dosanjh",
+        img: "diljit.jpg"
+    },
+    {
+        Name: "Tarsem Jassar",
+        img: "tarsem.jpg"
+    },
+    {
+        Name: "Shubh",
+        img: "shubh.jpg"
+    },
+    {
+        Name: "Gippy Grewal",
+        img: "gippy.jpg"
+    }
+]
+
+let data3 = [
+    {
+        songName: "Bitch_i'm_Back.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Bitch i'm Back",
+        image: "bitch.jpg"
+    },
+    {
+        songName: "Regret.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Regret",
+        image: "regret.jpg"
+    },
+    {
+        songName: "Vaar.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Vaar",
+        image: "vaar.jpg"
+    },
+    {
+        songName: "Sin.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Sin",
+        image: "sin.jpg"
+    },
+    {
+        songName: "Burberry.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Burberry",
+        image: "burberry.jpg"
+    },
+    {
+        songName: "Moosetape_Intro.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Moosetape Intro",
+        image: "moosetape.jpg"
+    },
+    {
+        songName: "US.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "US",
+        image: "us.jpg"
+    },
+    {
+        songName: "Unfuckwithable.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Unfuckwithable",
+        image: "unfuck.jpg"
+    },
+    {
+        songName: "Racks_And_Rounds.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "Racks And Rounds",
+        image: "rack.jpg"
+    },
+    {
+        songName: "295.mp3",
+        singerName: "Sidhu Moose Wala",
+        nickName: "295",
+        image: "295.jpg"
+    },
+    {
+        songName: "Birthday_Bash_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Birthday Bash",
+        image: "birthday.jpg"
+    },
+    {
+        songName: "Call_Aundi_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Call Aundi",
+        image: "call.jpg"
+    },
+    {
+        songName: "Saiyaan_Ji_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Saiyaan Ji",
+        image: "saiyaan.webp"
+    },
+    {
+        songName: "Love_Dose_Extended.mp3",
+        singerName: "Honey Singh",
+        nickName: "Love Dose Extended",
+        image: "love.jpg"
+    },
+    {
+        songName: "Jaam_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Jaam",
+        image: "Jaam.jpg"
+    },
+    {
+        songName: "Paris_Ka_Trip_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Paris Ka Trip",
+        image: "Paris.jpg"
+    },
+    {
+        songName: "Raat_Jashan_Di_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Raat Jashan Di",
+        image: "Raat.jpg"
+    },
+    {
+        songName: "Together_Forever_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Together",
+        image: "Together.jpg"
+    },
+    {
+        songName: "Yaar_Bathere_4.mp3",
+        singerName: "Honey Singh",
+        nickName: "Yaar Bathere",
+        image: "yaar.jpg"
+    },
+    {
+        songName: "Note_Fenko_1.mp3",
+        singerName: "Honey Singh",
+        nickName: "Note Fenko",
+        image: "Note.jpg"
+    },
+    {
+        songName: "Gaddi_Pichhe_Naa_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Gaddi Pichhe Naa",
+        image: "gaddi.jpg"
+    },
+    {
+        songName: "Sun_Fer_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Sun Fer",
+        image: "sun.jpg"
+    },
+    {
+        songName: "Chak_Chak_Chak_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Chak Chak Chak",
+        image: "chak.jpg"
+    },
+    {
+        songName: "Churi_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Churi",
+        image: "Churi.jpg"
+    },
+    {
+        songName: "2_Cheene_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "2 Cheene",
+        image: "2Cheene.jpg"
+    },
+    {
+        songName: "Bille_Bille_Naina_Waliye.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Bille Bille Naina Waliye",
+        image: "Bille.jpg"
+    },
+    {
+        songName: "Naam_Sunuga_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Naam Sunuga",
+        image: "Naam.jpg"
+    },
+    {
+        songName: "Nakhro_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Nakhro",
+        image: "Nakhro.jpg"
+    },
+    {
+        songName: "Numb_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Numb",
+        image: "numb.jpg"
+    },
+    {
+        songName: "Surma_1.mp3",
+        singerName: "Khan Bhaini",
+        nickName: "Surma",
+        image: "Surma.jpg"
+    }
+]
